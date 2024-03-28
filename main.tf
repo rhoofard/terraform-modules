be996dc7d814d5914e249aec570b81efcac640db
Ryan Hoofard <42755382+rhoofard@users.noreply.github.com>
2024-03-28 14:59:56 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,63 @@
-resource "random_integer" "fault" {
-  min = 0
-  max = 9
-  keepers = {
-    bloop = var.bloop
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.0"
+    }
   }
 }
-
-
-locals {
-  even_odd = tostring(random_integer.fault.result % 2)
+provider "kubernetes" {
+  config_path = "~/.kube/config"
 }
-
-resource "null_resource" "cluster" {
-  triggers = {
-    result = random_integer.fault.result
+resource "kubernetes_namespace" "test" {
+  metadata {
+    name = "nginx"
   }
-
-  provisioner "local-exec" {
-    command = "bash -c 'exit ${var.bleep}'"
+}
+resource "kubernetes_deployment" "test" {
+  metadata {
+    name      = "nginx"
+    namespace = kubernetes_namespace.test.metadata.0.name
+  }
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        app = "MyTestApp"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "MyTestApp"
+        }
+      }
+      spec {
+        container {
+          image = "nginx"
+          name  = "nginx-container"
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+resource "kubernetes_service" "test" {
+  metadata {
+    name      = "nginx"
+    namespace = kubernetes_namespace.test.metadata.0.name
+  }
+  spec {
+    selector = {
+      app = kubernetes_deployment.test.spec.0.template.0.metadata.0.labels.app
+    }
+    type = "NodePort"
+    port {
+      node_port   = 30201
+      port        = 80
+      target_port = 80
+    }
   }
 }
