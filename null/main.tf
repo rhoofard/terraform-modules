49075d5f16f3d613efa500e6c2d338b23261c4aa
rhoofard <42755382+rhoofard@users.noreply.github.com>
2024-04-09 14:16:26 -0700

    referencing remote module

--- null/main.tf
+++ null/main.tf
@@ -1,22 +1,5 @@
-resource "random_integer" "fault" {
-  min = 0
-  max = 9
-  keepers = {
-    bloop = var.bloop
-  }
-}
-
-
-locals {
-  even_odd = tostring(random_integer.fault.result % 2)
-}
-
-resource "null_resource" "cluster" {
-  triggers = {
-    result = random_integer.fault.result
-  }
-
-  provisioner "local-exec" {
-    command = "ls -al"
-  }
+module "new_name" {
+ source = "git@github.com:Pactionly/sample-tf-module.git?ref=v1.1.0"
+ bloop = "jibberis"
+ bleep = 0
 }
