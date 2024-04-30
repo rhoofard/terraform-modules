f6485503b10cc15c28b144b4a8cfb7e49d27bf1d
Ryan Hoofard <42755382+rhoofard@users.noreply.github.com>
2024-04-29 18:02:18 -0700

    Update main.tf

--- null/main.tf
+++ null/main.tf
@@ -1,5 +1,5 @@
 module "source_module" {
- source = "github.com/rhoofard/sample-tf-module?ref=v1.4.0"
+ source = "github.com/rhoofard/sample-tf-module?ref=v1.5.0"
  bloop = "jibberis"
  bleep = 0
 }
