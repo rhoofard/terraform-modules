cd8acf04796721507392d24bcb82290014610442
rhoofard <42755382+rhoofard@users.noreply.github.com>
2024-05-17 10:08:39 -0700

    Update main.tf

--- null3/main.tf
+++ null3/main.tf
@@ -1,5 +1,5 @@
 module "sample_module2_no_error" {
- source = "github.com/rhoofard/sample-tf-module2?ref=v1.1.0"
+ source = "github.com/rhoofard/sample-tf-module2?ref=v1.3.0"
  bloop = "jibberis"
- bleep = 0
+ bleep = 1
 }
